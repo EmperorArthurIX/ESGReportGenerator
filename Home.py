fda373856f727b17d65ca18e876e537514ffc3c9
gb07bh <gaurav193693@gmail.com>
2023-08-24 21:41:56 +0530

    fixed output

--- Home.py
+++ Home.py
@@ -8,10 +8,15 @@ st.title("ESG Report Generation")
 # Sidebar Configuration
 st.sidebar.header("Search Parameters")
 orgString = st.sidebar.text_input("Organisation", placeholder="Example & Sons")
-startDate = st.sidebar.date_input("Start Year", help="The starting year for the report data")
-endDate = st.sidebar.date_input("End Year", help="The closing year for the report data")
+startDate = st.sidebar.date_input(
+    "Start Year", help="The starting year for the report data")
+endDate = st.sidebar.date_input(
+    "End Year", help="The closing year for the report data")
 PSID = st.sidebar.text_input("Secure1_PSID Cookie", type="password")
 PSIDTS = st.sidebar.text_input("Secure1_PSIDTS Cookie", type="password")
+generate_doc = st.sidebar.button(label="Generate Document")
+# download_doc = st.sidebar.button(label = "Download Generated Document")
+
 
 # Variables
 queryString = "Write an ESG report for the company {} based on data from {} to {}"
@@ -26,4 +31,12 @@ st.write(queryString.format(orgString, startDate.year, endDate.year))
 if orgString and startDate and endDate and PSID and PSIDTS:
     from LLMUtils import BardAPIConsumer
     llm = BardAPIConsumer(PSID=PSID, PSIDTS=PSIDTS)
-    st.write(llm.get_report_text(queryString.format(orgString, startDate.year, endDate.year)))
+    st.write(llm.get_report_text(queryString.format(
+        orgString, startDate.year, endDate.year)))
+    if generate_doc:
+        llm.export_word(llm.get_report_text(
+            queryString.format(orgString, startDate.year, endDate.year)))
+        with open("report.docx", 'r+') as file:
+            if st.download_button("Download Generated Document", file_name='report.docx', data=file):
+                # st.download_button()
+                st.success("Downloading File!")
--- LLMUtils.py
+++ LLMUtils.py
@@ -14,15 +14,54 @@ class BardAPIConsumer:
         self.bard = BardCookies(cookie_dict=cookie_dict)
 
     def get_report_text(self, queryString) -> str:
-        report = self.bard.get_answer(queryString)
+        report = self.bard.get_answer(queryString)['content']
         return report
 
     def export_word(self, reportText: str):
-        pass
+        from docx import Document
+        from docx.shared import Pt, RGBColor
+        value = reportText
+        doc = Document()
+
+        # Define styles for headings and paragraphs
+        # 1 corresponds to paragraph style
+        heading_style = doc.styles.add_style('HeadingStyle', 1)
+        heading_font = heading_style.font
+        heading_font.bold = True
+        heading_font.size = Pt(16)
+        heading_style.paragraph_format.alignment = 3  # Justify alignment
+        heading_font.color.rgb = RGBColor(51, 51, 51)
+
+        # 1 corresponds to paragraph style
+        body_style = doc.styles.add_style('BodyStyle', 1)
+        body_style.paragraph_format.alignment = 3  # Justify alignment
+
+        # Process sections and add them to the document
+        sections = value.split('**')
+        for i in range(1, len(sections), 2):
+            heading_text = sections[i].strip()
+            content_text = sections[i + 1].strip()
+
+            if heading_text and content_text:
+                # Add heading
+                heading = doc.add_paragraph(heading_text, style='HeadingStyle')
+
+                # Add content with bullet points
+                content_lines = content_text.split('\n')
+                for line in content_lines:
+                    if line.strip().startswith('* '):
+                        bullet_item = doc.add_paragraph(
+                            line.strip()[2:], style='BodyStyle').style = 'ListBullet'
+                    else:
+                        doc.add_paragraph(line.strip(), style='BodyStyle')
+
+        # Save the document
+        doc.save('report.docx')
+        # pass
 
     def image_desc(self):
         pass
-    
+
     def refresh_cookies(self, PSID, PSIDTS):
         self.PSID = PSID
         self.PSIDTS = PSIDTS
