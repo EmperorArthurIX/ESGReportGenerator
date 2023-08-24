59b6da8f7891c39507fc711b43eb891c1a52759e
Ishaan Sunita Pandita <emperorarthurix@gmail.com>
2023-08-24 21:21:06 +0530

    Testing Integration of Utils

--- Home.py
+++ Home.py
@@ -8,10 +8,8 @@ st.title("ESG Report Generation")
 # Sidebar Configuration
 st.sidebar.header("Search Parameters")
 orgString = st.sidebar.text_input("Organisation", placeholder="Example & Sons")
-startDate = st.sidebar.date_input(
-    "Start Year", help="The starting year for the report data")
-endDate = st.sidebar.date_input(
-    "End Year", help="The closing year for the report data")
+startDate = st.sidebar.date_input("Start Year", help="The starting year for the report data")
+endDate = st.sidebar.date_input("End Year", help="The closing year for the report data")
 PSID = st.sidebar.text_input("Secure1_PSID Cookie", type="password")
 PSIDTS = st.sidebar.text_input("Secure1_PSIDTS Cookie", type="password")
 
@@ -24,3 +22,8 @@ queryString = "Write an ESG report for the company {} based on data from {} to {
 st.header("Search Query")
 st.subheader("Your search query looks like this:")
 st.write(queryString.format(orgString, startDate.year, endDate.year))
+
+if orgString and startDate and endDate and PSID and PSIDTS:
+    from LLMUtils import BardAPIConsumer
+    llm = BardAPIConsumer(PSID=PSID, PSIDTS=PSIDTS)
+    st.write(llm.get_report_text(queryString.format(orgString, startDate.year, endDate.year)))
--- LLMUtils.py
+++ LLMUtils.py
@@ -6,13 +6,12 @@ class BardAPIConsumer:
     def __init__(self, PSID: str, PSIDTS: str):
         self.PSID = PSID
         self.PSIDTS = PSIDTS
-        self.bard = Bard()
         cookie_dict = {
             "__Secure-1PSID": PSID,
             "__Secure-1PSIDTS": PSIDTS,
             # Any cookie values you want to pass session object.
         }
-        model = BardCookies(cookie_dict=cookie_dict)
+        self.bard = BardCookies(cookie_dict=cookie_dict)
 
     def get_report_text(self, queryString) -> str:
         report = self.bard.get_answer(queryString)
@@ -23,6 +22,10 @@ class BardAPIConsumer:
 
     def image_desc(self):
         pass
+    
+    def refresh_cookies(self, PSID, PSIDTS):
+        self.PSID = PSID
+        self.PSIDTS = PSIDTS
 
 
 if __name__ == '__main__':
